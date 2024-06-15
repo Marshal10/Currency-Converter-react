adf224c8242847c59747dd10fc47306233e1cb18
Marshal10 <marshalmaleane123@gmail.com>
2024-06-15 23:57:51 +0530

    Loading state and same currency error handling

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ export default function App() {
   const [toCurr, setToCurr] = useState("USD");
   const [amt, setAmt] = useState(1);
   const [output, setOutput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(
     function () {
       async function fetchResult() {
+        setIsLoading(true);
         const res = await fetch(
           `https://api.frankfurter.app/latest?amount=${amt}&from=${fromCurr}&to=${toCurr}`
         );
         const data = await res.json();
         setOutput((o) => data.rates[`${toCurr}`]);
+        setIsLoading(false);
       }
+      if (fromCurr === toCurr) return setOutput(amt);
       fetchResult();
     },
     [amt, fromCurr, toCurr]
@@ -26,23 +30,31 @@ export default function App() {
         type="text"
         value={amt}
         onChange={(e) => setAmt((a) => +e.target.value)}
+        disabled={isLoading}
       />
       <select
         value={fromCurr}
         onChange={(e) => setFromCurr((c) => e.target.value)}
+        disabled={isLoading}
       >
         <option value="EUR">EUR</option>
         <option value="USD">USD</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value={toCurr} onChange={(e) => setToCurr((c) => e.target.value)}>
+      <select
+        value={toCurr}
+        onChange={(e) => setToCurr((c) => e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>{output}</p>
+      <p>
+        {output} {toCurr}
+      </p>
     </div>
   );
 }
