09ec26cf67162f7a99094e258583289cd480a21a
Marshal10 <marshalmaleane123@gmail.com>
2024-06-15 23:30:54 +0530

    App component and css stylings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 export default function App() {
   return (
-    <div className="app">
-      <h1>Hello React</h1>
+    <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
     </div>
   );
 }
